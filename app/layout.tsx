ab70f8b3e02c0fa28eab7cb4b38d11f6f61fcbe7
Saurav Gupta <er.sauravgpt@gmail.com>
2024-03-02 23:23:58 +0530

    clerk is wrapped outside the router

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,10 @@ import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark, neobrutalism, shadesOfPurple } from '@clerk/themes';
 
+//global layout across all routes
 
 const IBMPlex = IBM_Plex_Sans({ 
   subsets: ["latin"],
@@ -21,10 +24,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-    <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
-      {children}
-    </body>
-  </html>
+    <ClerkProvider
+    appearance={{
+   
+      variables: { colorPrimary: 'red' },
+      signIn: { 
+        
+        variables: { colorPrimary: 'blue' }
+      }
+    }}
+  >
+      <html lang="en">
+        <head>
+          {/* You can import your IBM Plex Sans font here */}
+        </head>
+        <body className={cn("font-IBMPlex antialiased")}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
-};
+}

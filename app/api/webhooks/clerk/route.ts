4bc35f277c414fc67e73fd2d19a4fb83efe64eaa
Saurav Gupta <er.sauravgpt@gmail.com>
2024-03-03 11:48:39 +0530

    minor canges

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -13,13 +13,11 @@ export async function POST(req: Request) {
     throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
   }
  
-  // Get the headers
   const headerPayload = headers();
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
  
-  console.log(headerPayload, " ", svix_id, " ", svix_timestamp," ", svix_signature )
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
     return new Response('Error occured -- no svix headers', {
@@ -45,11 +43,11 @@ export async function POST(req: Request) {
     }) as WebhookEvent
   } catch (err) {
     console.error('Error verifying webhook:', err);
-    return new Response('Error occured in connection', {
+    return new Response(' connection Error occured ', {
       status: 400
     })
   }
- 
+  
   // Get the ID and type
   const { id } = evt.data;
   const eventType = evt.type;
